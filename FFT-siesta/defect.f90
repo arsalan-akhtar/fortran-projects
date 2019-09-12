334a9ae19e9c085eff6fc92e0e25efbd0270cdc4
arsalan-akhtar <arsalan_akhtar@outlook.com>
2019-09-12 18:28:13 +0200

    checking E_lat

--- FFT-siesta/defect.f90
+++ FFT-siesta/defect.f90
@@ -48,7 +48,7 @@ integer  ::  iv,ns,n1,n2,n3,ic
 real(kind=8),parameter              ::  bohr_to_ang=0.529177
 real(kind=8), parameter :: ang_to_bohr=1.889726878
 !-----------------------------------------------------------------------
-real(kind=8)            :: E_lat,g2,ecut,E_lat_z,deltag,deltax,gn2 
+real(kind=8)            :: E_q_lat,E_q_lat_z,ecut,deltax,gn2 
 double precision        :: rcell_sub_charge(3,3),rcell_sub_neutral(3,3)!,r(3)
 integer                 :: q_x,q_y,q_z,numpoint,h,ign
 
@@ -56,6 +56,8 @@ integer                 :: q_x,q_y,q_z,numpoint,h,ign
 !Declaration For E_lattice
 !-----------------------------------------------------------------------
 real(kind=4)            :: epsi
+integer                 :: charge
+real(kind=8)            :: E_r_lat
 !-----------------------------------------------------------------------
 !Declaration For V_lr
 !-----------------------------------------------------------------------
@@ -68,13 +70,14 @@ real(kind=8) ,allocatable,dimension(:,:)  ::  f_sub_kind_neutral
 real(kind=8) ,allocatable,dimension(:,:)  ::  f_sub_kind_charge
 real(grid_p), pointer :: CG_neutral(:,:)
 real(grid_p), pointer :: CG_charge(:,:)
+real(grid_p), pointer :: V_lr_p(:,:)
 !integer               ::      nsm
 !-----------------------------------------------------------------------
 !Declaration For Functions
 !-----------------------------------------------------------------------
-real(kind=8)   ::ev_to_k,q_model
+real(kind=8)   :: ev_to_k,q_model
 real(kind=4)   :: q_model_r,r_norm,gamma_d,beta_d,x_d
-real(kind=8)   :: gamma2,beta2,x
+real(kind=8)   :: gamma2,beta2,x,deltag,g2
 integer(kind=8) :: Vl_d,i_x,i_s,i2,i3,ip,is,ind,counter,r_0(3),r_x,r_y,r_z
 !=======================================================================
 ! INPUTS & User Defined Values
@@ -88,15 +91,20 @@ task_sub_neutral='read'
 task_sub_charge='read'
 nsm_sub_neutral=1
 nsm_sub_charge=1
-ecut=ev_to_k(2400.0_dp)
+ecut=ev_to_k(5000000.0_dp)
 epsi=20.0
 x_d=0.0
+x=0.0
 gamma_d=1.0
-beta_d=1.0*ang_to_bohr
+gamma2=1.0
+beta_d=2.0 !*(10**(-5))!*ang_to_bohr
+beta2=2.0!*(10**(-5))
 r_0(1)=1
 r_0(2)=1
 r_0(3)=1
 ns=1
+charge=-2
+
 !=======================================================================
 !-----------------------------------------------------------------------
 !READING Neutral VT
@@ -139,6 +147,9 @@ write (*,*) "the number of spins (nspin_sub) :" , nspin_sub_neutral
 write (*,*) "the nubmer of mesh point (mesh_sub):" , mesh_sub_neutral
 write (*,*) "the number of sub mesh point (nsm) :" , nsm_sub_neutral
 write (*,*) "the number of maximum mesh point (maxp) :" , maxp_sub_neutral
+volume_neutral = VOLCEL( cell_sub_neutral )
+write(*,*)"The Volume is  :",volume_neutral ," A^3"
+
 !-----------------------------------------------------------------------
 ! Charge READING INFO
 !-----------------------------------------------------------------------
@@ -153,6 +164,9 @@ write (*,*) "the number of spins (nspin_sub) :" , nspin_sub_charge
 write (*,*) "the nubmer of mesh point (mesh_sub):" , mesh_sub_charge
 write (*,*) "the number of sub mesh point (nsm) :" , nsm_sub_charge
 write (*,*) "the number of maximum mesh point (maxp) :" , maxp_sub_charge
+volume_charge = VOLCEL( cell_sub_charge )
+write(*,*)"The Volume is  :",volume_charge ," A^3"
+
 !-----------------------------------------------------------------------
 ! Allocating For V_neutral,V_charge
 !-----------------------------------------------------------------------
@@ -243,7 +257,6 @@ end do
 nsm=nsm_sub_charge
 !!$OMP end parallel do
 call fft( CG_neutral, mesh_sub_neutral, -1 )
-
 write(*,*) "FT"
 do ic=1,maxp_sub_neutral
 !    write(*,*) ic,CG_neutral(1,ic)
@@ -253,7 +266,9 @@ deallocate(f_sub_kind_charge)
 !-----------------------------------------------------------------------
 !Calculating V^{lr}
 !-----------------------------------------------------------------------
+write(*,*) "============================================================"
 write(*,*) "Calculating V^{lr}"
+write(*,*) "============================================================"
 !call de_alloc(V_lr)
 !Vl_d=(1600)!maxp_sub_charge!(160*160*160*160)
 !deallocate(V_lr)
@@ -271,33 +286,105 @@ do is = 1,nspin_sub_charge
      do i3 = 1,mesh_sub_charge(3)
           do i2 = 1,mesh_sub_charge(2)
                do ip=1,mesh_sub_charge(1)
-                    r_x=abs(ip-r_0(1))
-                    r_y=abs(i2-r_0(2))
-                    r_z=abs(i3-r_0(3))
-                    !r_norm = (r_x**2+r_y**2+r_z**2)**(1.0/2.0)
-                    r_norm = (r_x+r_y+r_z)
+!                    r_x=abs(ip-r_0(1))
+!                    r_y=abs(i2-r_0(2))
+!                    r_z=abs(i3-r_0(3))
+!                    r_norm = (r_x**2+r_y**2+r_z**2)**(1.0/2.0)
+!                    r_norm = (r_x+r_y+r_z)
                     !V_lr(ind+ip,is)=0.0  ! For testing *************
-                    V_lr(ind+ip,is)= (1/epsi)*q_model_r(x_d,gamma_d,beta_d,ip,i2,i3,r_0)/r_norm
-                    
-                    write (*,*) ind+ip,ip,i2,i3,is, V_lr(ind+ip,is)
+                    V_lr(ind+ip,is)= (1/epsi)*q_model_r(charge,x_d,gamma_d,beta_d,ip,i2,i3,r_0)!/r_norm                    
+!                    write (*,*) ind+ip,ip,i2,i3,is, V_lr(ind+ip,is)
                     counter=counter+1
                end do
                ind = ind + mesh_sub_charge(1)
           end do
      end do    
 end do
-!deallocate(V_lr)
-!call de_alloc(V_lr)
+!-----------------------------------------------------------------------
+!Calculating V^{lr}_periodic  
+!-----------------------------------------------------------------------
+nullify( V_lr_p )
+call re_alloc( V_lr_p, 1, 2, 1, mesh_sub_charge(1)*mesh_sub_charge(2)*mesh_sub_charge(3))
+!write(*,*) "Test"
+do ic=1,maxp_sub_charge
+   V_lr_p(1,ic) = V_lr(ic,1)
+   V_lr_p(1,ic) = V_lr(ic,1)
+!   write(*,*)ic,V_lr(ic,1)    !For testing
+   V_lr_p(2,ic) = 0.0_grid_p
+end do
+write(*,*) "============================================================"
+write(*,*) "Calculating FFT of V^{lr}                                   "
+write(*,*) "============================================================"
+call fft( V_lr_p, mesh_sub_charge, -1 )
+do ic=1,maxp_sub_charge
+!    write(*,*) ic,V_lr_p(1,ic)    
+end do
+call reclat( cell_sub_charge,rcell_sub_charge,1 )
+!-----------------------------------------------------------------------
+!Finding norm (modules) of Reciprocal Cell-vector 
+!-----------------------------------------------------------------------
+do iv = 1,3
+   rcell_sub_charge_md(iv) = dot_product(rcell_sub_charge(:,iv),rcell_sub_charge(:,iv))
+   rcell_sub_charge_md(iv) = sqrt(rcell_sub_charge_md(iv))
+enddo
+counter=0
+write(*,*) "============================================================"
+write(*,*) "Calculating V^{lr}_{periodic} Divide by |G|^2"
+write(*,*) "============================================================"
+do q_x=1,mesh_sub_charge(1)
+     do q_y=1,mesh_sub_charge(2)
+          do q_z=1,mesh_sub_charge(3)
+               g2=(sqrt((rcell_sub_charge_md(1)*q_x)**2+(rcell_sub_charge_md(2)*q_y)**2&
+&                    +(rcell_sub_charge_md(1)*q_z )**2))              
+               counter=counter+1
+               V_lr_p(1,counter)=(4*pi*V_lr_p(1,counter))/((epsi)*g2**2)
+!               write(*,*) counter,g2,V_lr_p(1,counter)
+          enddo
+     enddo
+end do
+!-----------------------------------------------------------------------
+!Back FFT of  V_lr_p
+!-----------------------------------------------------------------------
+write(*,*) "============================================================"
+write(*,*) "Back FFT of V^{lr}_{periodic}"
+write(*,*) "============================================================"
+call fft( V_lr_p, mesh_sub_charge, +1 )
+do ic=1,maxp_sub_charge
+!    write(*,*) ic,V_lr_p(1,ic)    
+end do
+!-----------------------------------------------------------------------
+!Fixing the dimension of V_lr_p
+!-----------------------------------------------------------------------
+counter=0
+do is = 1,nspin_sub_charge
+     ind = 0
+     counter=0
+     do q_x=1,mesh_sub_charge(3)
+          do q_y=1,mesh_sub_charge(2)
+               do q_z=1,mesh_sub_charge(1)
+                    counter=counter+1
+                    V_lr_p(ind+q_z,is)=V_lr_p(1,counter) 
+!               write(*,*) counter,V_lr_p(1,counter)
+!               write(*,*) counter,V_lr_p(ind+q_z,is)               
+               enddo
+               ind = ind + mesh_sub_charge(1)
+          enddo
+     enddo
+end do
+!call de_alloc(V_lr_p)
 !-----------------------------------------------------------------------
 !Calculating G Vector
 !-----------------------------------------------------------------------
 !write (*,*)"Charge", q_model(0.0_dp,1.0_dp,1.0_dp,1.0_dp)
 call reclat( cell_sub_neutral,rcell_sub_neutral,1 )
-write(*,*) "The G VECTOR is  :",rcell_sub_neutral(1,1),rcell_sub_neutral(1,2),rcell_sub_neutral(1,3)        
-write(*,*) "The G VECTOR is  :",rcell_sub_neutral(2,1),rcell_sub_neutral(2,2),rcell_sub_neutral(2,3)         
-write(*,*) "The G VECTOR is  :",rcell_sub_neutral(3,1),rcell_sub_neutral(3,2),rcell_sub_neutral(3,3)          
+write(*,*) "============================================================"
+write(*,*) "The G VECTOR is  :"
+write(*,100) rcell_sub_neutral(1,1),rcell_sub_neutral(1,2),rcell_sub_neutral(1,3)        
+write(*,100) rcell_sub_neutral(2,1),rcell_sub_neutral(2,2),rcell_sub_neutral(2,3)         
+write(*,100) rcell_sub_neutral(3,1),rcell_sub_neutral(3,2),rcell_sub_neutral(3,3)          
 volume_neutral = VOLCEL( cell_sub_neutral )
 write(*,*)"The Volume is  :",volume_neutral ," A^3"
+write(*,*) "============================================================"
 !-----------------------------------------------------------------------
 !Finding norm (modules) of Reciprocal Cell-vector 
 !-----------------------------------------------------------------------
@@ -318,16 +405,20 @@ do q_x=1,int(ecut)
 &               +(rcell_sub_neutral_md(1)*q_z )**2)) < ecut ) then
                     g2=(sqrt((rcell_sub_neutral_md(1)*q_x)**2+(rcell_sub_neutral_md(1)*q_y)**2&
 &                    +(rcell_sub_neutral_md(1)*q_z )**2))
-!                    write(*,*)  g2,rcell_sub_md(1)*q_x , rcell_sub_md(1)*q_y,rcell_sub_md(1)*q_z 
+!                    write(*,*)  g2,rcell_sub_neutral_md(1)*q_x ,&
+!&                    rcell_sub_neutral_md(1)*q_y,rcell_sub_neutral_md(1)*q_z 
                end if
           enddo
      enddo
 end do
 !-----------------------------------------------------------------------
-!Calculating E_lat
+!Calculating E_lat(q) 
 !-----------------------------------------------------------------------
-E_lat=0.0
-E_lat_z=0.0
+write(*,*) "============================================================"
+write(*,*) "Calculating  E_lattice(q)"
+write(*,*) "============================================================"
+E_q_lat=0.0
+E_q_lat_z=0.0
 deltag=0.0
 do q_x=1,int(ecut)
      do q_y=1,int(ecut)
@@ -336,30 +427,55 @@ do q_x=1,int(ecut)
 &               +(rcell_sub_neutral_md(1)*q_z )**2)) < ecut ) then
                     g2=(sqrt((rcell_sub_neutral_md(1)*q_x)**2+(rcell_sub_neutral_md(1)*q_y)**2 &
 &                    +(rcell_sub_neutral_md(1)*q_z )**2))
-                    E_lat= E_lat+((q_model(x,gamma2,beta2,g2)**2)/(g2**2))
-!                    V_lr(q_)                    
-!                    write(*,*) q_model(x,gamma2,beta2,g2),E_lat*((2*pi)/(epsi*VOLUME ))
-                    
+                    E_q_lat= E_q_lat+((q_model(x,gamma2,beta2,g2)**2)/(g2**2))
+!                    write(*,*) q_model(x,gamma2,beta2,g2)!,E_q_lat*((2*pi)/(epsi*volume_neutral ))
+!                    write(*,*) E_q_lat !*((2*pi)/(epsi*volume_neutral ))
+!                    write(*,*) q_model(x,gamma2,beta2,g2)**2                    
                end if
           enddo
      enddo
 end do
-numpoint=1000000
+!write(*,*) E_q_lat*((2*pi)/(epsi*volume_neutral))
+numpoint=500
 deltag=ecut/numpoint
+write(*,*) "E_cut is ",ecut, "Delta g is",deltag
 do ign=0,numpoint
-     E_lat_z= E_lat_z + deltag*(q_model(x,gamma2,beta2,deltag)**2)              
+     E_q_lat_z= E_q_lat_z + (q_model(x,gamma2,beta2,deltag)**2)              
+!     write(*,*) ign,deltag,q_model(x,gamma2,beta2,deltag)**2,(E_q_lat_z)
      deltag=ign+deltag
-!     write(*,*) ign,(E_lat_z*(1/pi*(epsi)))
+!     write(*,*) ign,deltag,(E_q_lat_z*(1/pi*(epsi)))
 enddo
-write(*,*) "TOTAL E_Lattice", ((E_lat*((2*pi)/(epsi*volume_neutral )))-(E_lat_z*(1/pi*(epsi))))
+!write(*,*) "Total E_Lattice(g) =", ((E_q_lat*((2*pi)/(epsi*volume_neutral ))))
+write(*,*) "Total E_Lattice(g) with g=0 =", E_q_lat*((2*pi)/(epsi*volume_neutral))
+write(*,*) "Total Removing g=0 with it self ",E_q_lat_z*(1.0/pi*(epsi))! *deltag
+write(*,*) "Total E_Lattice(g)-without g=0 =", E_q_lat*((2*pi)/(epsi*volume_neutral))- E_q_lat_z*(1/pi*(epsi))!*deltag
+!write(*,*) "Total E_Lattice(g) =", ((E_q_lat*((2*pi)/(epsi*volume_neutral )))-(E_q_lat_z*(1/pi*(epsi))))
 !                    E_lat_z= E_lat_z + deltag*(q_model(x,gamma2,beta2,g2)**2)              
 !                    deltag=g2-deltag
 !,(E_lat_z*(1/pi*(epsi)))
-
-!write(*,100)
-!f=q_model(0.0_dp,1.0_dp,1.0_dp,g2)
-
-
+!-----------------------------------------------------------------------
+!Calculating E_lat(r) 
+!-----------------------------------------------------------------------
+write(*,*) "============================================================"
+write(*,*) "Calculating  E_lattice(r)"
+write(*,*) "============================================================"
+ind = 0
+E_r_lat=0.0
+deltax=(volume_neutral/(maxp_sub_charge))
+write(*,*) volume_charge
+do i3 = 1,mesh_sub_charge(3)
+     do i2 = 1,mesh_sub_charge(2)
+          do ip=1,mesh_sub_charge(1)
+               E_r_lat=E_r_lat+(0.5*(q_model_r(charge,x_d,gamma_d,beta_d,ip,i2,i3,r_0) &
+& + ((-1)*charge/volume_neutral))*(V_lr_p(ip+ind,1)-V_lr(ip+ind,1)) &
+& + ((-1)*charge/volume_neutral)*V_lr(ip+ind,1))               
+!          write(*,*) E_r_lat
+          end do
+          ind = ind + mesh_sub_charge(1)
+     end do
+end do    
+write(*,*) "Total E_lattice (r) =", E_r_lat
+write(*,*) "Total E_lattice (r) =", E_r_lat*deltax
 
 
 
@@ -367,6 +483,7 @@ write(*,*) "TOTAL E_Lattice", ((E_lat*((2*pi)/(epsi*volume_neutral )))-(E_lat_z*
 
 deallocate(f_sub_neutral)
 deallocate(f_sub_charge)
+call de_alloc(V_lr_p)
 !deallocate(f_sub_kind_neutral)
 !deallocate(f_sub_kind_charge)
 deallocate(V_lr)
@@ -509,7 +626,7 @@ end Function q_model
 !-----------------------------------------------------------------------
 !Q MODEL(r-r_0) Functions
 !-----------------------------------------------------------------------
-real(kind=4) Function q_model_r(x,gamma_d,beta_d,r1,r2,r3,r_0)
+real(kind=4) Function q_model_r(charge_d,x,gamma_d,beta_d,r1,r2,r3,r_0)
      use units,   only : pi
 !    "Gaussian Model in PyCDT"
 !    "q(r)=q[x exp(-r/gamma) +(1-x) exp(-r^2/beta^2)]"
@@ -523,14 +640,15 @@ real(kind=4) Function q_model_r(x,gamma_d,beta_d,r1,r2,r3,r_0)
      implicit none
      real(kind=4)          :: gamma_d,beta_d,x,N_gamma,N_beta
      real(kind=4)          :: a,b,c
-     integer(kind=8)              :: r_0(3),r1,r2,r3  
+     integer(kind=8)       :: r_0(3),r1,r2,r3
+     integer               :: charge_d 
      N_gamma=8*pi*gamma_d**3
      N_beta=pi**(3.0/2.0)*beta_d**3
      a=abs(r1-r_0(1))
      b=abs(r2-r_0(2))
      c=abs(r3-r_0(3))
-     q_model_r=x*N_gamma**(-1)*exp((-1)*sqrt(a**2+b**2+c**2)/beta_d)+&
-&     (1-x)*N_beta**(-1)*exp((-1)*(sqrt(a**2+b**2+c**2))**2/beta_d**2)
+     q_model_r=charge_d*x*N_gamma**(-1)*exp((-1)*sqrt(a**2+b**2+c**2)/beta_d)+&
+&    charge_d*(1-x)*N_beta**(-1)*exp((-1)*(sqrt(a**2+b**2+c**2))**2/beta_d**2)
      !q_model_r=(exp((-1)*a)**2)/beta_d**2
 end Function q_model_r
 
